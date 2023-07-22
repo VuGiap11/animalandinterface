793b31b1bbcf5ac7c560232895e4f4c23b105ec6
vu giap <vudinhgiap11@gmail.com>
2023-07-22 17:24:31 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace animalandinterfaceEdible
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            animal[] animals = new animal[2];
+            animals[0] = new tiger();
+            animals[1] = new Chicken();
+
+            foreach (animal animal in animals)
+            {
+                Console.WriteLine(animal.Makesound());
+
+                if (animal is Chicken)
+                {
+                    Edible edibler = (Chicken)animal;
+                    Console.WriteLine(edibler.howtoEat());
+                }
+            }
+        }
+    }
+}
---
+++ animal.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace animalandinterfaceEdible
+{
+
+    abstract class animal
+    {
+        public abstract string Makesound();
+
+    }
+    class tiger : animal
+    {
+        public override string Makesound()
+        {
+            return "Tiger: roarrrrr!";
+        }
+    }
+    class Chicken : animal, Edible
+    {
+        public string howtoEat()
+        {
+            return "could be fried";
+        }
+
+        public override string Makesound()
+        {
+            return "Chicken: cluck-cluck!";
+        }
+    }
+    abstract class Fruit : Edible
+    {
+        public abstract string howtoEat();
+    }
+    class Orange : Fruit
+    {
+        public override string howtoEat()
+        {
+            return "Orange could be juiced";
+        }
+    }
+    class Apple : Fruit
+    {
+        public override string howtoEat()
+        {
+            return "Apple could be slided";
+        }
+    }
+}
